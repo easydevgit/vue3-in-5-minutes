b049abcb783362631ab7c0a738c1b95f0883e9bf
easydevgit <urserg1@mail.com>
2023-02-19 19:48:25 +0300

    Initial commit

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,28 @@
+<script setup>
+const props = defineProps({
+  // props go here
+  count: Number
+})
+
+const emit = defineEmits(['update'])
+
+function buttonClick() {
+  emit('update')
+}
+
+</script>
+
+<template>
+  <div class="test-component">
+    <h1>Test Component {{count}}</h1>
+    <button @click="buttonClick"> Event from Child Component </button>
+  </div>
+</template>
+
+<style scoped>
+.test-component {
+  text-align: center;
+  font-size: 36px;
+  color: blueviolet;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<script setup>
+import TestComponent from '../components/TestComponent.vue'
+import { onMounted, reactive, ref } from 'vue'
+
+const count = reactive({ number: 1 })
+
+function increment() {
+  count.number++
+}
+
+onMounted(() => {
+  console.log(`The initial count is ${count.number}.`)
+})
+
+function catchUpdate() {
+  console.log(`Event from child component received.`)
+}
+
+</script>
+
+<template>
+  <main>
+    <TestComponent :count="count.number" @update="catchUpdate" ></TestComponent>
+    Hello Vue
+    <button @click="increment"> Add 1 to count </button>
+  </main>
+</template>
